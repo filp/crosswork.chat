ecc10981bd621f06899342ef6b4fece256f16401
Filipe Dobreira <dobreira@gmail.com>
2018-02-12 21:10:08 +0000

    Add RoC

--- src/components/Conduct.vue
+++ src/components/Conduct.vue
@@ -6,9 +6,35 @@
           <h2>Rules of conduct</h2>
 
           <p>
-            TBD
+            To ensure the community runs smoothly, please do your best to stick to the following rules of conduct.
+            For anything not explicitly laid out in these topics, use your common sense, and treat others as you'd
+            like to be treated yourself.
           </p>
         </header>
+
+        <section class="rules">
+          <h4>Don't be a jerk</h4>
+          <p>
+            Be respectful and civil towards others. No harassment, trolling, and hate speech (including
+            but not limited to ethnicity, age, gender, sexual orientation, nationality, and religion).
+          </p>
+        </section>
+
+        <section class="rules">
+          <h4>Stay on topic</h4>
+          <p>
+            Casual conversations are very welcome, but disruptive arguments are not. Take it to private messaging. Please
+            do your best to stay within the topic of each channel.
+          </p>
+        </section>
+
+        <section class="rules">
+          <h4>No excessive advertisement, recruitment, etc</h4>
+          <p>
+            This isn't the place to sell your product, or run your recruitment efforts. It's OK to show
+            what you've been working on, but use your common sense.
+          </p>
+        </section>
       </div>
     </div>
   </div>
