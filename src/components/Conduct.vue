461d73ad515eeeda03a5723febeb2bb56b74ec75
Filipe Dobreira <dobreira@gmail.com>
2018-02-12 16:30:21 +0000

    Add empty rules page

---
+++ src/components/Conduct.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page page-conduct">
+    <div class="row">
+      <div class="col-xs-12">
+        <header class="rules rules-header">
+          <h2>Rules of conduct</h2>
+
+          <p>
+            TBD
+          </p>
+        </header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Conduct',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style  lang="scss">
+</style>
